1790931b20e6f1c2a3f8a76facf92a9bb7d02ab4
Kaustav <kkhatua@deloitte.com>
2022-07-04 14:28:15 +0000

    Fixed typo in code_uploader

--- Pipeline_Builder/Helper_Functions/.ipynb_checkpoints/code_uploader-checkpoint.py
+++ Pipeline_Builder/Helper_Functions/.ipynb_checkpoints/code_uploader-checkpoint.py
@@ -22,7 +22,7 @@ subprocess.run(["aws", "s3", "cp", f"evaluation.tar.gz", f"s3://{build_parameter
 subprocess.run(["aws", "s3", "cp", f"SageMaker_Pipeline_Component_Codes/Training/{build_parameters['processing_code_file_name']}", f"s3://{build_parameters['input_bucket']}/codes/"])
 subprocess.run(["aws", "s3", "cp", f"SageMaker_Pipeline_Component_Codes/Training/{build_parameters['get_best_model_code_file_name']}", f"s3://{build_parameters['input_bucket']}/codes/"])
 subprocess.run(["aws", "s3", "cp", f"{build_parameters['scoring_preprocessing_code_location']}", f"s3://{build_parameters['input_bucket']}/codes/"])
-subprocess.run(["cp", build_parameters["scoring_code_loaction"], "tmp_targz"])
+subprocess.run(["cp", build_parameters["scoring_code_location"], "tmp_targz"])
 subprocess.run(["tar", "-czvf", "inference.tar.gz", "-C", "tmp_targz", build_parameters['scoring_code_loaction'].split('/')[-1]])
 subprocess.run(["aws", "s3", "cp", f"inference.tar.gz", f"s3://{build_parameters['input_bucket']}/codes/"])
 subprocess.run(["aws", "s3", "cp", build_parameters['endpoint_updater_code_location'], f"s3://{build_parameters['input_bucket']}/codes/"])
