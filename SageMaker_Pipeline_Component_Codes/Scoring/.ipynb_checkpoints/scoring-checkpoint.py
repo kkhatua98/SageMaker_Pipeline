e962553ad23c223f5eb0dfcbabeef122e1d1f3f6
Kaustav <kkhatua@deloitte.com>
2022-07-12 11:14:07 +0000

    Incorporated given model in Training Pipeline

--- SageMaker_Pipeline_Component_Codes/Scoring/.ipynb_checkpoints/scoring-checkpoint.py
+++ SageMaker_Pipeline_Component_Codes/Scoring/.ipynb_checkpoints/scoring-checkpoint.py
@@ -9,12 +9,12 @@ for content in data_folder_contents:
         data_file_name = content
 scoring_data = pd.read_csv(f"/opt/ml/processing/input/data/{data_file_name}")
 
-dir_list = os.listdir("/opt/ml/processing/input/model_folder")
-    for content in dir_list:
-        if "joblib" in content:
-            file_name = content
+dir_list = os.listdir(model_folder)
+for content in dir_list:
+    if "joblib" in content:
+        file_name = content
 print(dir_list)
-preprocessor = joblib.load(os.path.join("/opt/ml/processing/input/model_folder", file_name))
+preprocessor = joblib.load(f"{model_folder}/{file_name}")
 
 predictions = preprocessor.predict(scoring_data)
 predictions.to_csv("/opt/ml/processing/train/Predictions.csv")
--- SageMaker_Pipeline_Component_Codes/Scoring/scoring.py
+++ SageMaker_Pipeline_Component_Codes/Scoring/scoring.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import os
+import subprocess
+
 data_folder = "/opt/ml/processing/input/data"
 model_folder = "/opt/ml/processing/input/model_folder"
 
@@ -9,12 +11,19 @@ for content in data_folder_contents:
         data_file_name = content
 scoring_data = pd.read_csv(f"/opt/ml/processing/input/data/{data_file_name}")
 
-dir_list = os.listdir("/opt/ml/processing/input/model_folder")
-    for content in dir_list:
-        if "joblib" in content:
-            file_name = content
+dir_list = os.listdir(model_folder)
+for content in dir_list:
+    if "tar.gz" in content:
+        zip_file_name = content
+subprocess.run(["cp", f"{model_folder}/{zip_file_name}", '.'])
+subprocess.run(["tar", "-xf", f"{zip_file_name}"])
 print(dir_list)
-preprocessor = joblib.load(os.path.join("/opt/ml/processing/input/model_folder", file_name))
+
+dir_list = os.listdir('.')
+for content in dir_list:
+    if "joblib" in content:
+        file_name = content
+preprocessor = joblib.load(f"{file_name}")
 
 predictions = preprocessor.predict(scoring_data)
 predictions.to_csv("/opt/ml/processing/train/Predictions.csv")
