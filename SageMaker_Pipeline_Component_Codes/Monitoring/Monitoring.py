7ab2a4c0d96a805fbb76ceb35fb18b7d2c1babfc
Kaustav <kkhatua@deloitte.com>
2022-07-25 10:56:42 +0000

    Moved s3 dependency to Monitor.py

--- SageMaker_Pipeline_Component_Codes/Monitoring/Monitoring.py
+++ SageMaker_Pipeline_Component_Codes/Monitoring/Monitoring.py
@@ -8,6 +8,12 @@ def monitoring_function():
     from sklearn.metrics import f1_score
     from sklearn.metrics import roc_curve
     import glob
+    import subprocess
+    import sys
+    
+    subprocess.run(["pip", "install", "fsspec==2022.5.0", "s3fs==2022.5.0"])
+    import s3fs
+    s3 = s3fs.S3FileSystem(anon=False)
 
     import argparse
     parser = argparse.ArgumentParser()
@@ -17,7 +23,9 @@ def monitoring_function():
     args, _ = parser.parse_known_args()
 
     y_actual = pd.read_csv(glob.glob(f"{args.y_actual_location}/*.csv")[0]).loc[:, "Churn"]
-    y_predicted = pd.read_csv(glob.glob(f"{args.y_predicted_location}/*.csv")).loc[:, "Prediction"]
+    y_predicted_s3 = f"s3://{s3.glob(args.y_predicted_location)[0]}"
+    # y_predicted = pd.read_csv(glob.glob(f"{args.y_predicted_location}/*.csv")).loc[:, "Prediction"]
+    y_predicted = pd.read_csv(y_predicted_s3)
 
     tn, fp, fn, tp = confusion_matrix(y_actual,y_predicted).ravel()
     accuracy = round(100*accuracy_score(y_actual,y_predicted),2)
